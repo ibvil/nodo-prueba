aaea37a7f0d7dc5382a5950d2f013e55bc1c2ae0
ibarVilches <ib.vilches@gmail.com>
2018-11-23 19:56:33 -0300

    Desarrollo Prueba P1 Ibar Vilches

--- P1/logic-test.js
+++ P1/logic-test.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6 */
 // No editar
 const teams = [
   { id: 1, country: 'Spain', name: 'Real Madrid C.F.' },
@@ -14,7 +15,7 @@ const teams = [
   { id: 12, country: 'Italy', name: 'Juventus FC' },
   { id: 13, country: 'France', name: 'Olympique Marseille' }
 
-]
+];
 
 const leagues = [
   { id: 1, country: 'England', name: 'Premier League' },
@@ -24,7 +25,7 @@ const leagues = [
   { id: 5, country: 'Italy', name: 'Serie A' },
   { id: 6, country: 'Portugal', name: 'Liga NOS' },
   { id: 7, country: 'France', name: 'Lige 1' }
-]
+];
 
 const teamsByLeague = [
   { teamId: 12, leagueId: 5 },
@@ -40,7 +41,7 @@ const teamsByLeague = [
   { teamId: 11, leagueId: 2 },
   { teamId: 1, leagueId: 4 },
   { teamId: 13, leagueId: 7 }
-]
+];
 
 const winsByTeams = [
   { teamId: 10, wins: 2 },
@@ -56,7 +57,7 @@ const winsByTeams = [
   { teamId: 11, wins: 1 },
   { teamId: 12, wins: 2 },
   { teamId: 13, wins: 1 }
-]
+];
 
 /*
   SECCIÓN PROBLEMAS
@@ -70,49 +71,132 @@ const winsByTeams = [
 
 // 0 Arreglo con los ids de los equipos (función de ejemplo)
 function listTeamsIds () {
-  return teams.map((client) => client.id)
+  return teams.map((client) => client.id);
 }
 
 // 1 Arreglo con los nombres de los equipos y el país al que pertenecen, ordenados alfabéticamente por el nombre de su país de origen.
-function listTeamsByCountry () {
-  // CODE HERE
+function listTeamsByCountry() {
+  return teams.sort((a, b) => a.country > b.country ? 1 : b.country > a.country ? -1 : 0)
+    .map((team) => team.name + ' ' + team.country);
 }
 
 // 2 Arreglo con los nombres de los equipos ordenados de mayor a menor por la cantidad de victorias en champions league.
-function sortTeamsByWins () {
-  // CODE HERE
+function sortTeamsByWins() {
+  let array = [];
+
+  teams.forEach(team => {
+    let wins = winsByTeams.filter(winsByTeam => winsByTeam.teamId == team.id).map((winsByTeam) => winsByTeam.wins);
+    array.push({
+      name: team.name,
+      wins: wins.reduce((total, winsByTeam) => total + winsByTeam, 0)
+    });
+  });
+
+  return array.sort((a, b) => a.wins > b.wins ? -1 : b.wins > a.wins ? 1 : 0).map((client) => client.name);
 }
 
 // 3 Arreglo de objetos en donde se muestre el nombre de las ligas y la sumatoria de las victorias de los equipos que pertenecen a ellas.
 function leaguesWithWins () {
-  // CODE HERE
+  let array = [];
+
+  leagues.forEach( league => {
+    let leagueTeams = teamsByLeague.filter(team => team.leagueId == league.id).map((team) => team.teamId);
+    let wins = winsByTeams.filter(winsByTeam => leagueTeams.includes(winsByTeam.teamId)).map((winsByTeam) => winsByTeam.wins);
+
+    array.push({
+      name: league.name,
+      wins: wins.reduce((total, winsByTeam) => total + winsByTeam, 0)
+    });
+
+  });
+
+  return array;
+}
+//Funcion Auxiliar - retorna arreglo de objetos con teamId y wins por liga
+function sortTeamsByWinsForLeague (leagueId) {
+  let leagueTeams = teamsByLeague.filter(team => team.leagueId == leagueId).map((team) => team.teamId);
+  let winsByTeam = winsByTeams.filter(winsByTeam => leagueTeams.includes(winsByTeam.teamId))
+    .sort((a, b) => a.wins > b.wins ? 1 : b.wins > a.wins ? -1 : 0);
+  return winsByTeam;
 }
 
 // 4 Objeto en que las claves sean los nombres de las ligas y los valores el nombre del equipo con la menor cantidad de victorias en champions.
 function leaguesWithTeamWithLestWins () {
-  // CODE HERE
+  let object = {};
+
+  leagues.forEach(league => {
+
+    let winsByTeam =  sortTeamsByWinsForLeague(league.id);
+
+    object[league.name] = teams.find( team => team.id == winsByTeam[0].teamId ).name;
+
+  });
+
+  return object;
 }
 
 // 5 Objeto en que las claves sean los nombres de las ligas y los valores el nombre del equipo con la mayor cantidad de victorias en champions.
 function leaguesWithTeamWithMostWins () {
-  // CODE HERE
+  let object = {};
+
+  leagues.forEach(league => {
+
+    let winsByTeam =  sortTeamsByWinsForLeague(league.id);
+
+    object[league.name] = teams.find( team => team.id == winsByTeam[ winsByTeam.length-1 ].teamId ).name;
+
+  });
+
+  return object;
 }
 
 // 6 Arreglo con los nombres de las ligas ordenadas de mayor a menor por la cantidad de victorias de sus equipos.
 function sortLeaguesByTeamsByWins () {
-  // CODE HERE
+  return leaguesWithWins().sort((a, b) => a.wins > b.wins ? -1 : b.wins > a.wins ? 1 : 0).map((league) => league.name);
 }
 
 // 7 Arreglo con los nombres de las ligas ordenadas de mayor a menor por la cantidad de equipos que participan en ellas.
 function sortLeaguesByTeams () {
-  // CODE HERE
+  let array = [];
+
+  leagues.forEach(league => {
+
+    let numberOfTeams = teamsByLeague.filter(team => team.leagueId == league.id).length;
+
+    array.push({
+      name: league.name,
+      numberOfTeams: numberOfTeams
+    });
+
+  });
+
+  return array.sort((a, b) => a.numberOfTeams > b.numberOfTeams ? -1 : b.numberOfTeams > a.numberOfTeams ? 1 : 0).map((league) => league.name);
 }
 
 // 8 Agregar un nuevo equipo con datos ficticios a "teams", asociarlo a la liga de Francia y agregar un total de 4 victorias en champions.
 // Luego devolver el lugar que ocupa este equipo en el ranking de la pregunta 2.
 // No modificar arreglos originales para no alterar las respuestas anteriores al correr la solución
 function newTeamRanking () {
-  // CODE HERE
+  let teamId = teams.length + 1;
+  let teamName = 'IbVilches F.C.';
+
+  teams.push({
+    id: teamId,
+    country: 'France',
+    name: teamName
+  });
+  teamsByLeague.push({
+    teamId: teamId,
+    leagueId: 7
+  });
+  winsByTeams.push({
+    teamId: teamId,
+    wins: 4
+  });
+
+  let array = sortTeamsByWins();
+
+  return array.indexOf(teamName) + 1;
 }
 
 // 9 Realice una función que retorne una promesa con los nombres de los equipos en upper case.
@@ -120,33 +204,40 @@ function newTeamRanking () {
 // recuerde que debe esperar el retorno de función asíncrona para que su resultado pueda ser mostrado por el
 // console.log. Utilice async await para la llamada asíncrona a la función.
 // NOTA: solo debe crear la función asíncrona y agregar la llamada en la siguiente función.
-async function getTeamsNamesAsUpperCase () {
-  let response
+/* jshint ignore:start */
+async function getTeamsNamesAsUpperCase() {
+  let response;
   // ------MAKE AWAIT CALL HERE------
-
+  response = await teamsNamesAsUpperCase();
   // --------------------------------
-  console.log('response:')
-  console.log(response)
+  console.log('response:');
+  console.log(response);
+}
+// Función que retorna Una promesa Ejercicio 9
+function teamsNamesAsUpperCase() {
+  return new Promise((resolve, reject) => {
+    resolve(teams.map(team => team.name.toUpperCase()));
+  });
 }
 
 // Impresión de soluciones. No modificar.
-console.log('Pregunta 0')
-console.log(listTeamsIds())
-console.log('Pregunta 1')
-console.log(listTeamsByCountry())
-console.log('Pregunta 2')
-console.log(sortTeamsByWins())
-console.log('Pregunta 3')
-console.log(leaguesWithWins())
-console.log('Pregunta 4')
-console.log((leaguesWithTeamWithLestWins()))
-console.log('Pregunta 5')
-console.log((leaguesWithTeamWithMostWins()))
-console.log('Pregunta 6')
-console.log((sortLeaguesByTeamsByWins()))
-console.log('Pregunta 7')
-console.log((sortLeaguesByTeams()))
-console.log('Pregunta 8')
-console.log((newTeamRanking()))
-console.log('Pregunta 9')
-console.log(getTeamsNamesAsUpperCase())
+console.log('Pregunta 0');
+console.log(listTeamsIds());
+console.log('Pregunta 1');
+console.log(listTeamsByCountry());
+console.log('Pregunta 2');
+console.log(sortTeamsByWins());
+console.log('Pregunta 3');
+console.log(leaguesWithWins());
+console.log('Pregunta 4');
+console.log((leaguesWithTeamWithLestWins()));
+console.log('Pregunta 5');
+console.log((leaguesWithTeamWithMostWins()));
+console.log('Pregunta 6');
+console.log((sortLeaguesByTeamsByWins()));
+console.log('Pregunta 7');
+console.log((sortLeaguesByTeams()));
+console.log('Pregunta 8');
+console.log((newTeamRanking()));
+console.log('Pregunta 9');
+console.log(getTeamsNamesAsUpperCase());
